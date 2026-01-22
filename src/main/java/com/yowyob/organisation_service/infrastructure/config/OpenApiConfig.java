b1abf8d772b10d2060deb617d21560875f0e3e76
KamgaBrayan <brayanarmel@gmail.com>
2026-01-22 12:59:03 +0100

    Complete Organisation API without CORS

--- src/main/java/com/yowyob/organisation_service/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/yowyob/organisation_service/infrastructure/config/OpenApiConfig.java
@@ -1,37 +1,39 @@
 package com.yowyob.organisation_service.infrastructure.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server; // <--- Import
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "Organisation API", version = "1.0.0", description = "Documentation de l'API Organisation"), servers = {
+                @Server(url = "http://localhost:8081", description = "Serveur Local") // <--- AJOUT IMPORTANT
+})
+
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
+        @Bean
+        public OpenAPI customOpenAPI() {
+                final String securitySchemeName = "bearerAuth";
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Organisation API")
-                        .version("1.0.0")
-                        .description("Documentation de l'API Organisation (Reactive)"))
-                
-                // 1. Active la sécurité sur toutes les routes
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                
-                // 2. Configure le schéma pour dire à Swagger : "C'est du HTTP Bearer"
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP) 
-                                        .scheme("bearer")              
-                                        .bearerFormat("JWT")
-                        ));
-    }
+                return new OpenAPI()
+                        .info(new io.swagger.v3.oas.models.info.Info()
+                                .title("Organisation API")
+                                .version("1.0.0")
+                                .description("Documentation de l'API Organisation (Reactive)"))
+                                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                                .components(new Components()
+                                                .addSecuritySchemes(securitySchemeName,
+                                                                new io.swagger.v3.oas.models.security.SecurityScheme()
+                                                                                .name(securitySchemeName)
+                                                                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")));
+        }
 }
