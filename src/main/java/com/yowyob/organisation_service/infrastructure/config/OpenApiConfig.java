8fc76c38f8f760daace72fbd5474106abdff9044
KamgaBrayan <brayanarmel@gmail.com>
2026-01-22 16:53:29 +0100

    Final Organisation API

--- src/main/java/com/yowyob/organisation_service/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/yowyob/organisation_service/infrastructure/config/OpenApiConfig.java
@@ -12,13 +12,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Organisation API", version = "1.0.0"), servers = {
-                // L'URL relative "/" permet à Swagger d'utiliser le même domaine que celui dans
-                // la barre d'adresse
-                @Server(url = "/", description = "Serveur par défaut (Relatif)"),
-                // Optionnel : URL explicite de prod
+
                 @Server(url = "https://organization-service.pynfi.com", description = "Production"),
-                // Optionnel : URL locale
-                @Server(url = "http://localhost:8081", description = "Localhost")
+
 })
 public class OpenApiConfig {
 
